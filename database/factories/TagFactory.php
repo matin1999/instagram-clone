7ffb41a8f38e3dafbf856f07806f0e6b4b0cda2d
matin1999 <matinakhgari1@gmail.com>
2020-12-30 02:26:26 +0330

    Tag factory aded

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -2,17 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\Comment;
+use App\Models\Tag;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class CommentFactory extends Factory
+class TagFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Comment::class;
+    protected $model = Tag::class;
 
     /**
      * Define the model's default state.
@@ -22,7 +22,7 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            'path' => $this->faker->sentence(),
+            'title' => $this->faker->name,
         ];
     }
 }
