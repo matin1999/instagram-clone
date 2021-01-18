ca49754c984d0546d2fe3a3ccb3f89844d4a3628
matin1999 <matinakhgari1@gmail.com>
2021-01-18 04:49:10 +0330

    add message table

--- app/Models/User.php
+++ app/Models/User.php
@@ -111,8 +111,14 @@ class User extends Authenticatable
         return $this->hasMany(Comment::class);
     }
 
-    public function messages()
+    public function sent()
     {
-        return $this->hasMany(Message::class);
+        return $this->hasMany(Message::class, 'sender_id');
+    }
+
+    // A user can also receive a message
+    public function received()
+    {
+        return $this->hasMany(Message::class, 'sent_to_id');
     }
 }
---
+++ database/migrations/2021_01_18_011357_create_message_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('body');
+            $table->timestamps();
+            $table->foreignId('sender_id')
+                ->references('id')
+                ->on('users');
+
+            $table->foreignId('sent_to_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+}
