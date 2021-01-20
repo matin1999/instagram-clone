90a0ebbe2d34eb8f5e466f76d7ebd34df2a3f479
matin1999 <matinakhgari1@gmail.com>
2021-01-20 11:44:37 +0330

    rearrenge

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -9,11 +9,12 @@ class Search extends Component
 {
     public $search;
     public $results;
+
     public function render()
     {
-        $users=User::where('user_name', 'like', '%'.$this->search.'%')->with('image')->limit(6)->get();
-        $this->results=$users->toArray();
-        return view('livewire.search')->with('results',$this->results);
+        $users = User::where('user_name', 'like', '%' . $this->search . '%')->with('image')->limit(6)->get();
+        $this->results = $users->toArray();
+        return view('livewire.search')->with('results', $this->results);
 
     }
 }
